d52934a2c538b82d091ff3f5c4404d37443c82a4
Your Name <you@example.com>
2022-07-18 14:26:11 +0500

    add components

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import './App.css';
 import Welcome from "./components/Welcome";
 import Transiction from "./components/Transiction";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
@@ -13,7 +14,8 @@ function App() {
        <Welcome></Welcome>
        <div className="mt-5"></div>
        <Transiction></Transiction>
-    
+       <div className="mt-5"></div>
+       <Footer></Footer>
     </div>
   );
 }
