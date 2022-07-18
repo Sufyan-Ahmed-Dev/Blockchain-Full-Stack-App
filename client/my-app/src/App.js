1773bb54a7507116f281e5fc012b9321968a6234
Your Name <you@example.com>
2022-07-18 11:30:55 +0500

    import components

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,10 +1,18 @@
 import Navbar from "./components/Navbar"
 import './App.css';
+import Welcome from "./components/Welcome";
+import Transiction from "./components/Transiction";
 function App() {
   return (
     <div className="App">
       
        <Navbar></Navbar>
+       <div className="mt-5"></div>
+       <div className="mt-5"></div>
+       <div className="mt-5"></div>
+       <Welcome></Welcome>
+       <div className="mt-5"></div>
+       <Transiction></Transiction>
     
     </div>
   );
