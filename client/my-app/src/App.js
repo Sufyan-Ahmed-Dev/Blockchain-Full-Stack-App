f4d58f6ca96d6cdb9e10608ec6455901dbfe6b36
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-09-20 01:47:08 +0500

    adding some component

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Welcome from "./components/Welcome";
 import Transiction from "./components/Transiction";
 import Footer from "./components/Footer";
+import WhiteListUser from "./components/WhiteListUser";
+import WhiteListAdminMInting from "./components/WhiteListAdminMInting";
+import PublicMinting from "./components/PublicMinting";
 function App() {
   return (
     <div className="App">
@@ -12,6 +15,24 @@ function App() {
        <div className="mt-5"></div>
        <div className="mt-5"></div>
        <Welcome></Welcome>
+       
+       <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-4 col-md-6 col-sm-12">
+          <WhiteListUser></WhiteListUser>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12">
+          <WhiteListAdminMInting ></WhiteListAdminMInting>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12">
+          <PublicMinting></PublicMinting>
+          </div>
+        </div>
+       </div>
+       
+       
+      
+       
        <div className="mt-5"></div>
        <Transiction></Transiction>
        <div className="mt-5"></div>
