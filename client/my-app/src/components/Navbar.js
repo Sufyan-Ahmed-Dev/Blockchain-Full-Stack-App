6b72145d2f512c53d8557d5623efcb7e3d6752b4
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-09-19 20:54:47 +0500

    app name changed

--- client/my-app/src/components/Navbar.js
+++ client/my-app/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
  <header id="header" className="fixed-top">
     <div className="container d-flex align-items-center">
 
-      <h1 className="logo me-auto"><a href="index.html">BlockChain-App</a></h1>
+      <h1 className="logo me-auto"><a href="index.html">NFT Minting App</a></h1>
       {/* <!-- Uncomment below if you prefer to use an image logo --> */}
       {/* <!-- <a href="index.html" className="logo me-auto"><img src="assets/img/logo.png" alt="" className="img-fluid"></a>--> */}
 
@@ -17,16 +17,12 @@ function Navbar() {
           <li><a href="#about.html">About</a></li>
           <li><a href="#courses.html">Services</a></li>
           <li><a href="#trainers.html">Transiction</a></li>
-          <li><a href="#events.html">SignIn</a></li>
-        
-
-       
         </ul>
         <i className="bi bi-list mobile-nav-toggle"></i>
       </nav>
       {/* !-- .navbar --> */}
 
-      <a href="#courses.html" className="get-started-btn">Login</a>
+      <a href="#courses.html" className="get-started-btn">ConnectWallet</a>
 
     </div>
   </header>
