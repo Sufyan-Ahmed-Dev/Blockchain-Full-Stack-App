cd956c6bd1593406ebfc116d7199150c6ff3ae8a
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-09-19 20:55:14 +0500

    input same as func parameter

--- client/my-app/src/components/Welcome.js
+++ client/my-app/src/components/Welcome.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Welcome() {
-    function ConnectWallet(){}
+    function ConnectWallet(){
+        alert("Connect Wallet Function Work Pending")
+    }
 
-    function TransferMoney(){}
+    function TransferMoney(){
+        alert("NFT minting Func will be pending")
+    }
     
     return (
         <>
@@ -38,28 +42,28 @@ function Welcome() {
 
                                                         <div className="d-flex align-items-center mb-3 pb-1">
                                                             <i className="fas fa-cubes fa-2x me-3" ></i>
-                                                            <span className="h2 fw-bold mb-0">Transfer</span>
+                                                            <span className="h2 fw-bold mb-0">Minting</span>
                                                         </div>
 
                                                         {/* <h5 className="fw-normal mb-3 pb-3">Sign into your account</h5> */}
 
                                                         <div className="form-outline mb-3">
-                                                            <input type="text" id="form2Example17" className="form-control" placeholder='Enter Sender Address' />
+                                                            <input type="text" id="form2Example17" className="form-control" placeholder='Enter NFT token ID' />
                                                             {/* <label className="form-label" Htmlfor="form2Example17">Account Address</label> */}
                                                         </div>
 
                                                         <div className="form-outline mb-3">
-                                                            <input type="number" id="form2Example27" className="form-control " placeholder='Enter Ether'/>
+                                                            <input type="number" id="form2Example27" className="form-control " placeholder='Enter Metadata Hash'/>
                                                             {/* <label className="form-label" Htmlfor="form2Example27"> Balance</label> */}
                                                         </div>
 
                                                         <div className="form-outline mb-3">
-                                                            <input type="text" id="form2Example27" className="form-control " placeholder='Enter Message' />
+                                                            <input type="text" id="form2Example27" className="form-control " placeholder='Enter NFT name' />
                                                             {/* <label className="form-label" Htmlfor="form2Example27">Text Message</label> */}
                                                         </div>
 
                                                         <div className="pt-1 mb-4">
-                                                            <button className="btn btn-outline-success" type="button" onClick={TransferMoney}>Send</button>
+                                                            <button className="btn btn-outline-success" type="button" onClick={TransferMoney}>Mint NFT</button>
                                                         </div>
                                                     </form>
 
