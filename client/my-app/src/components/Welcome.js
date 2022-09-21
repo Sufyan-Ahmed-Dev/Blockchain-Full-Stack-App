eccae1dee276a6d0e770367159a55cd26cf4197d
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-09-21 12:39:04 +0500

    add first page card

--- client/my-app/src/components/Welcome.js
+++ client/my-app/src/components/Welcome.js
@@ -13,7 +13,7 @@ function Welcome() {
         <>
             <div className="container ">
                 <div className="row justify-content-center" >
-                    <div className="col">
+                    <div className="col-lg-6 col-md-6 col-sm-12">
                         {/* <!-- ======= Hero Section ======= --> */}
                         <div className="mt-lg-5"></div>
                         <div className="mt-lg-3"></div>
@@ -27,6 +27,45 @@ function Welcome() {
                         </section>
                         {/* <!-- End Hero --> */}
 
+                    </div>
+                    <div className='col-lg-6 col-md-6 col-sm-12 mt-5'>
+                    <div className="card" >
+                        <div className=" g-0">
+
+                            <div className=" align-items-center">
+                                <div className="card-body p-lg-3 p-4 text-black">
+
+                                    <form>
+
+                                        <div className="d-flex align-items-center mb-3 pb-1">
+
+                                            <span className="h4 fw-bold mb-0">Your Account Detail</span>
+                                        </div>
+
+                                        {/* <h5 className="fw-normal mb-3 pb-3">Sign into your account</h5> */}
+
+                                        <div className="form-outline mb-3">
+                                        <p className="form-control "> Owner Address : 0x</p> 
+                                        </div>
+
+                                        <div className="form-outline mb-3">
+                                           <p className="form-control ">Your Address : 0x </p>
+                                        </div>
+
+                                        <div className="form-outline mb-3">
+                                        <p className="form-control "> Total Balnce : 13.5789</p> 
+                                        </div>
+
+                                        <div className="pt-1 mb-4">
+                                            <button className="btn btn-outline-success" type="button" >Change Wallet</button>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                        {/* </div> */}
+                    </div>
                     </div>
                 </div>
             </div>
