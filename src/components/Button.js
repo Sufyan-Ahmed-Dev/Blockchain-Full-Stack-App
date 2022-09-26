1e969d71bac5cb4201843de4b9c98958f5461586
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-09-27 01:21:02 +0500

    adding some func

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,120 +1,141 @@
 import { ethers } from 'ethers';
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ContractABI from "./Contract/contractABI.json"
 
 
 function Button() {
 
-      // const [CheckSale , setCheckSale] = useState();
+      const [CheckSale, setCheckSale] = useState(false);
+      useEffect(() => {
+            if (window.ethereum) {
+
+                  window.ethereum.on("chainChanged", chainChanged);
+            }
+      }, []);
       // const ethPrivkey = "935a0ebd10c4babfd1cb46ffaf8d620d179db57e81a339cd09f9483e99c574ab"
 
       // const [checkPublicSales , setcheckPublicSales] = useState();
-       const data = "0x5CFFa40F9079b9a05Ad0347D215A005B26ABE25e";   
-       const providers = new ethers.providers.Web3Provider(window.ethereum);
-       const signer =  providers.getSigner();
-       const contract = new ethers.Contract(data , ContractABI , signer);
+      const data = "0x5CFFa40F9079b9a05Ad0347D215A005B26ABE25e";
+      const providers = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = providers.getSigner();
+      const contract = new ethers.Contract(data, ContractABI, signer);
 
-      const CheckSales = async()=>{
-        const Sales = await contract.CheckPublicsale();
-        console.log("Public Sales Are: ",Sales)
-      // console.log(CheckSale)
-}
+      const CheckSales = async () => {
+            const Sales = await contract.CheckPublicsale();
+            console.log("Public Sales Are: ", Sales)
+            setCheckSale(Sales);
+            console.log(CheckSale);
 
-     const CheckOwner = async()=>{
-       const Owner = await contract.owner();
-       console.log("Owner of the Contract",Owner)
-}
-async function DeActivePubSales(){
-      if(typeof window.ethereum  !== "undefined"){        
-            const sendTX = await contract.unActicePublic();
-            await sendTX.wait();
-            console.log(sendTX);
       }
-      else{
-            return("failed")
+
+      const CheckOwner = async () => {
+            const Owner = await contract.owner();
+            console.log("Owner of the Contract", Owner)
       }
-}
-async function ActivePubSales(){
-      if(typeof window.ethereum  !== "undefined"){        
-            const sendTX = await contract.acticePublic();
-            await sendTX.wait();
-            console.log(sendTX);
+      async function DeActivePubSales() {
+            if (typeof window.ethereum !== "undefined") {
+                  const sendTX = await contract.unActicePublic();
+                  await sendTX.wait();
+                  console.log(sendTX);
+            }
+            else {
+                  return ("failed")
+            }
       }
-      else{
-            return("failed")
+      async function ActivePubSales() {
+            if (typeof window.ethereum !== "undefined") {
+                  try {
+                        const sendTX = await contract.acticePublic();
+                        await sendTX.wait();
+                        console.log(sendTX);
+                  } catch {
+
+                  }
+            }
+
+            else {
+                  return ("failed")
+            }
       }
-}
-  return (
-      <>
-      <div className='container'>
 
-      <div className="section-title">
-            {/* <h2>Courses</h2> */}
-            <p>All Buttons</p>
-          </div>
-      
-      <div className="row justify-content-center  g-4 ">
-  
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" onClick={ActivePubSales} >ActivePsales</button>
-      </div>
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button"onClick={DeActivePubSales} >DactivePsale</button>
-      </div>
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" onClick={CheckSales}>CheckPsales</button>
-            {/* <p className='text-dark'> Value :: {CheckSale}</p> */}
-      </div>
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >Pause</button>
-      </div>
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >UnPause</button>
-      </div>
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >CheckPause</button>
-      </div>
+      const chainChanged = () => {
+            setCheckSale(false);
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >BaseURI</button>
-      </div>
+      };
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >CheckWLsales</button>
-      </div>
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >Tname</button>
-      </div>
+      return (
+            <>
+                  <div className='container'>
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >Tsymble</button>
-      </div>
+                        <div className="section-title">
+                              {/* <h2>Courses</h2> */}
+                              <p>All Buttons</p>
+                        </div>
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" onClick={CheckOwner} >Owner</button>
-  
-      </div>
+                        <div className="row justify-content-center  g-4 ">
 
-      <div className="pt-1 col" >
-            <button className="btn btn-outline-success" type="button" >MaxLimit</button>
-      </div>
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" onClick={ActivePubSales} >ActivePsales</button>
+                              </div>
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" onClick={DeActivePubSales} >DactivePsale</button>
+                              </div>
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" onClick={CheckSales}>CheckPsales</button>
+                                    <p className='text-dark'> Value :: {CheckSale}</p>
+                              </div>
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >Pause</button>
+                              </div>
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >UnPause</button>
+                              </div>
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >AdminLimit</button>
-      </div>
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >CheckPause</button>
+                              </div>
 
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >WuserLimit</button>
-      </div>
-      <div className="pt-1 col">
-            <button className="btn btn-outline-success" type="button" >PublicLimit</button>
-      </div>
-      </div>
-      </div>
-      </>
-  )
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >BaseURI</button>
+                              </div>
+
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >CheckWLsales</button>
+                              </div>
+
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >Tname</button>
+                              </div>
+
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >Tsymble</button>
+                              </div>
+
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" onClick={CheckOwner} >Owner</button>
+
+                              </div>
+
+                              <div className="pt-1 col" >
+                                    <button className="btn btn-outline-success" type="button" >MaxLimit</button>
+                              </div>
+
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >AdminLimit</button>
+                              </div>
+
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >WuserLimit</button>
+                              </div>
+                              <div className="pt-1 col">
+                                    <button className="btn btn-outline-success" type="button" >PublicLimit</button>
+                              </div>
+                        </div>
+                  </div>
+            </>
+      )
 }
 
 export default Button
