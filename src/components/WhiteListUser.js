30d6838aa7412535d78d1bfec208d60b98f2fb2c
Zeeshan Nawaz <zeeshanm.nawaz786@gmail.com>
2022-09-28 22:12:21 +0500

    Adding react from data to console

--- src/components/WhiteListUser.js
+++ src/components/WhiteListUser.js
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function WhiteListUser() {
+
+function WhiteListUser({ addLog }) {
+    const [tokenId, setTokenId] = useState('');
+    const [hash, setHash] = useState('');
+    const [NFTname, setNFTname] = useState('');
+
+
+    var tokenID = (event)=>{
+        setTokenId(event.target.value)
+      }
+    
+      var Hash = (event)=>{
+        setHash(event.target.value)
+      }
+      var nftName = (event)=>{
+        setNFTname(event.target.value)
+      }
+
+
+    var submit = (event) =>{
+        event.preventDefault()
+        var signUplist = []
+
+        var signUpData= {
+            ID: tokenId,
+            Hash:hash,
+            NFTNAME: NFTname
+        }
+
+        signUplist.push(signUpData)
+        console.log(signUplist)
+    }
+
     return (
         <>
 
@@ -13,32 +45,33 @@ function WhiteListUser() {
                             <div className=" align-items-center">
                                 <div className="card-body p-lg-3 p-4 text-black">
 
-                                    <form>
+
+                                <form onSubmit={submit}>
+
 
                                         <div className="d-flex align-items-center mb-3 pb-1">
-
                                             <span className="h4 fw-bold mb-0">WhiteListUser</span>
                                         </div>
 
                                         {/* <h5 className="fw-normal mb-3 pb-3">Sign into your account</h5> */}
 
                                         <div className="form-outline mb-3">
-                                            <input type="text" id="form2Example17" className="form-control" placeholder='Enter NFT token ID' />
+                                            <input name='tokenId' value={tokenId} onChange={tokenID} type="number" id="form2Example17" className="form-control" placeholder='Enter NFT token ID' />
                                             {/* <label className="form-label" Htmlfor="form2Example17">Account Address</label> */}
                                         </div>
 
                                         <div className="form-outline mb-3">
-                                            <input type="number" id="form2Example27" className="form-control " placeholder='Enter Metadata Hash' />
+                                            <input name='hash' value={hash} onChange={Hash} type="text" id="form2Example27" className="form-control " placeholder='Enter Metadata Hash' />
                                             {/* <label className="form-label" Htmlfor="form2Example27"> Balance</label> */}
                                         </div>
 
                                         <div className="form-outline mb-3">
-                                            <input type="text" id="form2Example27" className="form-control " placeholder='Enter NFT name' />
+                                            <input name='NFTname' value={NFTname} onChange={nftName} type="text" id="form2Example27" className="form-control " placeholder='Enter NFT name' />
                                             {/* <label className="form-label" Htmlfor="form2Example27">Text Message</label> */}
                                         </div>
 
                                         <div className="pt-1 mb-4">
-                                            <button className="btn btn-outline-success" type="button" >Mint NFT</button>
+                                            <button className="btn btn-outline-success" type="submit"  >Mint NFT</button>
                                         </div>
                                     </form>
 
