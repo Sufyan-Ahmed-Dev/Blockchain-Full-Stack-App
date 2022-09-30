aebeb2ecf285d87b219f74a037c21e83fd14849c
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-09-30 09:16:30 +0500

    balance show on screen

--- src/components/BalanceOf.js
+++ src/components/BalanceOf.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React,{useState} from 'react'
+import ContractABI from './Contract/contractABI.json'
+import { ethers } from 'ethers';
 
 function BalanceOf() {
+    
+    const [balance, setbalance] = useState('');
+    const [status, setStatus] = useState('');
+
+
+
+
+    var Balance = (event) => {
+        setbalance(event.target.value)
+    }
+
+
+    var submit = (event) => {
+        event.preventDefault()
+
+        if (balance === '') {
+            setStatus("Gives Proper Data")
+        }
+        else {
+            var Balance = balance
+        }
+
+
+        async function balanceOf() {
+            if (typeof window.ethereum !== 'undefined') {
+                setStatus("wait")
+                try {
+                    const data = "0xE3605683A1fcbb9dbe9D9823B3935C1802313534";
+                    const providers = new ethers.providers.Web3Provider(window.ethereum);
+                    const signer = providers.getSigner();
+                    const contract = new ethers.Contract(data, ContractABI, signer);
+                    const sendTX = await contract.balanceOf(Balance)
+                    // await sendTX.wait()
+                    const check = sendTX.toNumber()
+                    console.log(check)
+                    setStatus(check)
+
+                }
+                catch (err) {
+                    console.log(err)
+                    setStatus("Gives Proper Data")
+
+                }
+            }
+            else {
+                setStatus("Not Working")
+
+            }
+        }
+        balanceOf()
+
+
+    }
+
     return (
+
 
 
         <>
@@ -14,7 +71,7 @@ function BalanceOf() {
                             <div className=" align-items-center">
                                 <div className="card-body p-lg-3 p-4 text-black">
 
-                                    <form>
+                                    <form onSubmit={submit}>
 
                                         <div className="d-flex align-items-center mb-3 pb-1">
 
@@ -24,14 +81,15 @@ function BalanceOf() {
                                         {/* <h5 className="fw-normal mb-3 pb-3">Sign into your account</h5> */}
 
                                         <div className="form-outline mb-3">
-                                            <input type="text" id="form2Example17" className="form-control" placeholder='Enter your Address' />
+                                            <input  name='balance' value={balance} onChange={Balance}  type="text" id="form2Example17" className="form-control" placeholder='Enter your Address' />
                                             {/* <label className="form-label" Htmlfor="form2Example17">Account Address</label> */}
                                         </div>
 
 
 
                                         <div className="pt-1 mb-4">
-                                            <button className="btn btn-outline-success" type="button" >Check</button>
+                                            <button className="btn btn-outline-success" type="submit" >Check</button>
+                                            <p className='text-danger'>{status}</p>
                                         </div>
                                     </form>
 
