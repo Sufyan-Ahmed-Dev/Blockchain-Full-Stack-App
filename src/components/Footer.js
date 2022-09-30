491f1583309cda5f16249a032265b23b92523121
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-09-30 10:35:45 +0500

    footer class change to classname

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <>
 
-<footer class="text-center text-white bg-dark" >
+<footer className="text-center text-white bg-dark" >
   {/* <!-- Copyright --> */}
-  <div class="text-center text-white p-3" >
+  <div className="text-center text-white p-3" >
     © 2020 Copyright:
     <span>Sufyan Ahmed</span>   
      {/* <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a> */}
